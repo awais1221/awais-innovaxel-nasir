ea9f6336d3faf916298ee0899546515b1a3ccd30
awais1221 <awaisnasir121@gmail.com>
2025-07-13 13:17:44 +0500

    Wire form interface to views

--- shortener/views.py
+++ shortener/views.py
@@ -17,6 +17,68 @@ def generate_short_code(length=6):
     return ''.join(random.choices(characters, k=length))
 
 
+def form_interface(request):
+    context = {}
+
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        
+
+        # 1. Shorten
+        if action == 'shorten':
+            url = request.POST.get('url')
+            code = generate_short_code()
+            while ShortURL.objects.filter(short_code=code).exists():
+                code = generate_short_code()
+            obj = ShortURL.objects.create(url=url, short_code=code)
+            context['short_url'] = request.build_absolute_uri(f'/shorten/{obj.short_code}/')
+
+        # 2. Get Original
+        elif action == 'get':
+            code = request.POST.get('code')
+            try:
+                obj = ShortURL.objects.get(short_code=code)
+                obj.access_count += 1
+                obj.save()
+                context['original_url'] = obj.url
+            except ShortURL.DoesNotExist:
+                context['original_url'] = 'Not found.'
+
+        # 3. Update
+        elif action == 'update':
+            code = request.POST.get('code')
+            new_url = request.POST.get('url')
+            try:
+                obj = ShortURL.objects.get(short_code=code)
+                obj.url = new_url
+                obj.save()
+                context['updated_url'] = obj.url
+            except ShortURL.DoesNotExist:
+                context['updated_url'] = 'Short code not found.'
+
+        # 4. Delete
+        elif action == 'delete':
+            code = request.POST.get('code')
+            try:
+                obj = ShortURL.objects.get(short_code=code)
+                obj.delete()
+                context['deleted'] = code
+            except ShortURL.DoesNotExist:
+                context['deleted'] = 'Short code not found.'
+
+        # 5. Stats
+        elif action == 'stats':
+            code = request.POST.get('code')
+            try:
+                obj = ShortURL.objects.get(short_code=code)
+                context['stats'] = {'code': code, 'count': obj.access_count}
+            except ShortURL.DoesNotExist:
+                context['stats'] = {'code': code, 'count': 'Not found'}
+
+    return render(request, 'index.html', context)
+
+
+
 
 class CreateShortURL(APIView):
     def post(self, request):
